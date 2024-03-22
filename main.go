a10478d2ae2c89a8818761b0086ce2917d8febf6
nschnettler <niklas.schnettler@gmail.com>
2024-03-22 16:49:25 +0100

    More fixes

--- main.go
+++ main.go
@@ -133,7 +133,8 @@ func filterVariants(module, variant string, variantsMap gradle.Variants) (gradle
 	filteredVariants := gradle.Variants{}
 	for m, variants := range variantsMap {
 		for _, v := range variants {
-			if strings.ToLower(v) == "verifySnapshots"+variant {
+			fmt.Println(v)
+			if v == "verifySnapshots"+variant {
 				filteredVariants[m] = append(filteredVariants[m], v)
 			}
 		}
